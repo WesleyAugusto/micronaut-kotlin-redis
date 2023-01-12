fdd587b29637d2dba6c3481c6730b4e4927b48dd
WesleyAugusto <WesleyAugusto>
2023-01-12 21:57:16 +0000

    Initial commit

---
+++ src/test/kotlin/com/data/MicronautKotlinRedisTest.kt
@@ -0,0 +1,12 @@
+package com.data
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class MicronautKotlinRedisTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
